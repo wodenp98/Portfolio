67c2acc16204334d3e72342c7a407ca008909598
Paul VIGNERON <paulvigneron4698@gmail.Com>
2022-05-11 17:56:10 +0200

    Navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -19,12 +19,30 @@ const Header = () => {
   return (
     <header className={`${bg ? 'bg-tertiary h-20' : 'h-24'} flex item-center fixed top-0 w-full text-white z-10 transition-all duration-300`}>
         <div className='container mx-auto h-full flex items-center justify-between'>
+
             {/* logo */}
-            <a href="#">
-                <img src={Logo} alt="Logo Paul Vigneron"/>
-            </a>
+
+                <a href="#">
+                    <img src={Logo} alt="Logo Paul Vigneron"/>
+                </a>
 
             {/* Nav */}
+
+                <div className='hidden lg:block'>
+                    <Nav />
+                </div>
+
+            {/* Social */}
+
+                <div className='hidden lg:block'>
+                    <Social />
+                </div>
+            
+            {/* Nav Mobile */}
+
+                <div className='lg:hidden'>
+                    <NavMobile />
+                </div>
         </div>
     </header>
   )
