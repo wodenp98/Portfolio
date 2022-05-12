42f5aa0d3876e51e64ed80d0fe6fb240e3c37cbc
Paul VIGNERON <paulvigneron4698@gmail.Com>
2022-05-12 16:35:08 +0200

    Base du portfolio

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,51 +1,46 @@
-import React, {useState, useEffect} from 'react'
+import React, { useEffect, useState } from 'react';
 
-import Logo from "../assets/img/logo.svg"
-import Nav from "../components/Nav"
-import NavMobile from "../components/NavMobile"
-import Social from "../components/Social"
+// import components
+import Logo from '../assets/img/logo.svg';
+import Nav from '../components/Nav';
+import NavMobile from '../components/NavMobile';
+import Socials from './Socials';
 
 const Header = () => {
-
-  const [bg, setBg] = React.useState(false)
+  const [bg, setBg] = useState(false);
 
   useEffect(() => {
-      window.addEventListener('scroll', () => {
-          return window.scrollY > 50 ? setBg(true) : setBg(false)
-      })
-  })
-
+    window.addEventListener('scroll', () => {
+      return window.scrollY > 50 ? setBg(true) : setBg(false);
+    });
+  });
 
   return (
-    <header className={`${bg ? 'bg-tertiary h-20' : 'h-24'} flex item-center fixed top-0 w-full text-white z-10 transition-all duration-300`}>
-        <div className='container mx-auto h-full flex items-center justify-between'>
-
-            {/* logo */}
-
-                <a href="#">
-                    <img src={Logo} alt="Logo Paul Vigneron"/>
-                </a>
-
-            {/* Nav */}
-
-                <div className='hidden lg:block'>
-                    <Nav />
-                </div>
-
-            {/* Social */}
-
-                <div className='hidden lg:block'>
-                    <Social />
-                </div>
-            
-            {/* Nav Mobile */}
-
-                <div className='lg:hidden'>
-                    <NavMobile />
-                </div>
+    <header
+      className={`${
+        bg ? 'bg-tertiary h-20' : 'h-24'
+      } flex items-center fixed top-0 w-full text-white z-10 transition-all duration-300`}
+    >
+      <div className='container mx-auto h-full flex items-center justify-between'>
+        {/* logo */}
+        <a href='#'>
+          <img src={Logo} alt='' />
+        </a>
+        {/* nav */}
+        <div className='hidden lg:block'>
+          <Nav />
         </div>
+        {/* Socials */}
+        <div className='hidden lg:block'>
+          <Socials />
+        </div>
+        {/* nav mobile*/}
+        <div className='lg:hidden'>
+          <NavMobile />
+        </div>
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
